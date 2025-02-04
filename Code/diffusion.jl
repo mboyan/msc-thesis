d4579971dbb81d931ce2902ec8ac3acda9589d13
Boyan Mihaylov <mih.boyan@gmail.com>
2025-02-04 23:24:02 +0100

    Derivation of low-res discretisation scheme

--- Code/diffusion.jl
+++ Code/diffusion.jl
@@ -83,7 +83,7 @@ __precompile__(false)
         return max(a, b)
     end
 
-    function update_GPU(c_old, c_new, N, dtdx2, D, Db, spore_idx)
+    function update_GPU!(c_old, c_new, N, dtdx2, D, Db, spore_idx)
         """
         Update the concentration values on the lattice
         using the time-dependent diffusion equation.
@@ -127,18 +127,49 @@ __precompile__(false)
                 Ddtdx2front * front + Ddtdx2back * back
 
             c_new[idx...] = center + weighted_nbrs - (Ddtdx2bottom + Ddtdx2top + Ddtdx2left + Ddtdx2right + Ddtdx2front + Ddtdx2back) * center
-
-            # c_new[idx...] = c_old[idx...] + Ddtdx2 * (
-            #     c_old[mod1(idx[1] + 1, N), idx[2], idx[3]] + c_old[mod1(idx[1] - 1, N), idx[2], idx[3]] +
-            #     c_old[idx[1], mod1(idx[2] + 1, N), idx[3]] + c_old[idx[1], mod1(idx[2] - 1, N), idx[3]] +
-            #     c_old[idx[1], idx[2], mod1(idx[3] + 1, N)] + c_old[idx[1], idx[2], mod1(idx[3] - 1, N)] - 6 * c_old[idx...])
         end
     
         return nothing
     end
 
+    function update_GPU_low_res!(c_old, c_new, N, dtdx2, D, Db, spore_vol_idx, c_spore, tau)
+        """
+        Update the concentration values on the lattice
+        using the time-dependent diffusion equation.
+        inputs:
+            c_old (array) - the current state of the lattice
+            c_new (array) - the updated state of the lattice
+            N (int) - the number of lattice rows/columns
+            dtdx2 (float) - the update factor
+            D (float) - the diffusion constant
+            Db (float) - the diffusion constant through the spore
+            spore_vol_idx (tuple) - the indices of the volume containing the spore location
+            c_spore (float) - the concentration at the spore
+            tau (float) - characteristic time for permeation
+        """
+        i, j, k = CUDA.blockIdx().x, CUDA.blockIdx().y, CUDA.blockIdx().z
+        ti, tj, tk = CUDA.threadIdx().x, CUDA.threadIdx().y, CUDA.threadIdx().z
+    
+        # Determine the indices of the current cell
+        idx = ((i - 1) * blockDim().x + ti, (j - 1) * blockDim().y + tj, (k - 1) * blockDim().z + tk)
+    
+        # Update the concentration value
+        if 1 ≤ idx[1] ≤ N && 1 ≤ idx[2] ≤ N && 1 ≤ idx[3] ≤ N
+
+            center = c_old[idx...]
+
+            add_c = 0.0
+
+            if idx[1] == spore_vol_idx[1] && idx[2] == spore_vol_idx[2] && idx[3] == spore_vol_idx[3]
+                add_c = center .- (center .- c_spore) .* exp.(-t / tau)
+            end
+        end
+
+        return nothing
+
+
     function diffusion_time_dependent_GPU(c_init, t_max; D=1.0, Db=nothing, Ps=1.0, dt=0.005, dx=5, n_save_frames=100,
-        spore_idx=nothing, spore_idx_spacing=nothing, c_thresholds=nothing, bottom_arrangement=false)
+        spore_idx=nothing, spore_idx_spacing=nothing, c_thresholds=nothing)
         """
         Compute the evolution of a square lattice of concentration scalars
         based on the time-dependent diffusion equation.
@@ -154,9 +185,10 @@ __precompile__(false)
             spore_idx (tuple) - the indices of the spore location; defaults to nothing
             spore_spacing (int) - the spacing between spore indices along each dimension; defaults to nothing; if used, spore_idx is ignored
             c_thresholds (vector of float) - threshold values for the concentration; defaults to nothing
-            bottom_arrangement (bool) - whether the spores are arranged at the bottom of the lattice; defaults to false
         outputs:
-            u_evolotion (numpy.ndarray) - the states of the lattice at all moments in time
+            c_evolotion (array) - the states of the lattice at all moments in time
+            times (array) - the times at which the states were saved
+            t_thresholds (array) - the times at which the concentration crossed the threshold
         """
 
         @argcheck ndims(c_init) == 3 "c_init must be a 3D array"
@@ -166,7 +198,6 @@ __precompile__(false)
         # Set the spore index reference
         if !isnothing(spore_idx_spacing)
             spore_idx_ref = spore_idx_spacing
-            # if bottom_arrangement
         elseif !isnothing(spore_idx)
             spore_idx_ref = spore_idx
             println("3D simulation")
@@ -232,7 +263,125 @@ __precompile__(false)
             end
 
             # Update the lattice
-            @cuda threads=kernel_threads blocks=kernel_blocks update_GPU(c_A_gpu, c_B_gpu, N, dtdx2, D, Db, spore_idx_ref)
+            @cuda threads=kernel_threads blocks=kernel_blocks update_GPU!(c_A_gpu, c_B_gpu, N, dtdx2, D, Db, spore_idx_ref)
+            c_A_gpu, c_B_gpu = c_B_gpu, c_A_gpu
+
+            # Check for threshold crossing
+            if !isnothing(c_thresholds)
+                if thresh_ct < length(t_thresholds) && t_thresholds[thresh_ct] == 0 && CUDA.reduce(max_reduce_kernel, c_A_gpu) < c_thresholds[thresh_ct]
+                    t_thresholds[thresh_ct] = t * dt
+                    thresh_ct += 1
+                end
+            end
+        end
+
+        # Save final frame
+        c_evolution[save_ct, :, :, :] .= Array(c_A_gpu)
+        times[save_ct] = t_max
+
+        return c_evolution, times, t_thresholds
+    end
+
+    function diffusion_time_dependent_GPU_low_res(c_init, t_max; D=1.0, Db=nothing, Ps=1.0, A=150, V=125, dt=150, dx=25, n_save_frames=100,
+        spore_vol_idx=nothing, spore_vol_idx_spacing=nothing, c_thresholds=nothing)
+        """
+        Compute the evolution of a square lattice of concentration scalars
+        based on the time-dependent diffusion equation. A concentration source
+        smaller than the lattice resolution is injecting new concentration
+        according to a slow permeation scheme.
+        inputs:
+            c_init (vector of float) - the initial state of the lattice
+            t_max (int) - a maximum number of iterations
+            D (float) - the diffusion constant; defaults to 1
+            Db (float) - the diffusion constant through the spore; defaults to nothing
+            Ps (float) - the permeation constant through the spore barrier; defaults to 1
+            A (float) - the surface area of the spore; defaults to 150
+            V (float) - the volume of the spore; defaults to 125
+            dt (float) - timestep; defaults to 0.001
+            dx (float) - spatial increment; defaults to 0.005
+            n_save_frames (int) - determines the number of frames to save during the simulation; detaults to 100
+            spore_vol_idx (tuple) - the indices of the volume containing the spore location; defaults to nothing
+            spore_vol_spacing (int) - the spacing between spore volume indices along each dimension; defaults to nothing; if used, spore_vol_idx is ignored
+            c_thresholds (vector of float) - threshold values for the concentration; defaults to nothing
+        outputs:
+            c_evolotion (array) - the states of the lattice at all moments in time
+            times (array) - the times at which the states were saved
+            t_thresholds (array) - the times at which the concentration crossed the threshold
+        """
+
+        @argcheck ndims(c_init) == 3 "c_init must be a 3D array"
+
+        GC.gc()
+
+        # Set the spore index reference
+        if !isnothing(spore_vol_idx_spacing)
+            spore_vol_idx_ref = spore_vol_idx_spacing
+        elseif !isnothing(spore_idx)
+            spore_vol_idx_ref = spore_idx
+            println("3D simulation")
+        else
+            println("Either spore_vol_idx or spore_vol_idx_spacing must be provided.")
+            return
+        end
+
+        # Determine number of lattice rows/columns
+        N = size(c_init)[1]
+        H = size(c_init)[3]
+
+        # Save update factor
+        dtdx2 = dt / (dx^2)
+
+        # Correction factor for permeation
+        if isnothing(Db)
+            Db = Ps * dx
+        end
+        println("Using D = $D, Db = $Db, Ps = $Ps")
+
+        # Check stability
+        if  D * dtdx2 ≥ 0.2
+            println("Warning: inappropriate scaling of dx and dt due to D, may result in an unstable simulation.")
+        end
+        if  Db * dtdx2 ≥ 0.2
+            println("Warning: inappropriate scaling of dx and dt due to Db, may result in an unstable simulation.")
+        end
+
+        # Determine number of frames
+        n_frames = Int(floor(t_max / dt))
+
+        # Allocate arrays for saving data
+        c_evolution = zeros(n_save_frames + 1, N, N, H)
+        times = zeros(n_save_frames + 1)
+        println("Storage arrays allocated.")
+        save_interval = floor(n_frames / n_save_frames)
+        save_ct = 1
+
+        # Allocate arrays for saving threshold crossing times
+        if isnothing(c_thresholds)
+            t_thresholds = nothing
+        else
+            t_thresholds = zeros(length(c_thresholds))
+        end
+        thresh_ct = 0
+
+        # Initialise lattice states on GPU
+        c_A_gpu = cu(c_init)
+        c_B_gpu = CUDA.zeros(N, N, H)
+
+        kernel_blocks, kernel_threads = invoke_smart_kernel_3D(size(c_init))
+
+        # Run the simulation
+        for t in 1:n_frames
+            
+            # Save frame
+            if t % save_interval == 0 && save_ct ≤ n_save_frames
+                c_evolution[save_ct, :, :, :] .= Array(c_A_gpu)
+                times[save_ct] = t * dt
+                save_ct += 1
+                println("Frame $save_ct saved.")
+            end
+
+            # Update the lattice
+            @cuda threads=kernel_threads blocks=kernel_blocks update_GPU_low_res!(c_A_gpu, c_B_gpu, N, dtdx2, D, Db, spore_vol_idx_ref)
             c_A_gpu, c_B_gpu = c_B_gpu, c_A_gpu
 
             # Check for threshold crossing
