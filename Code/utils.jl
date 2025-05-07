2d52f2e06fde1db693dbdc521db947f1e6219366
Boyan Mihaylov <mih.boyan@gmail.com>
2025-05-07 16:31:27 +0200

    Updates on model extensions
    Fitting of half-saturation probabilities, description of cell-wall adsorption model, introduction of inducer-inhibitor interaction section.

---
+++ Code/utils.jl
@@ -0,0 +1,69 @@
+module Utils
+    """
+    Contains utility functions.
+    """
+
+    using DataFrames
+    using CSV
+    
+    include("./conversions.jl")
+    using .Conversions
+
+    export parse_ijadpanahsaravi_data
+
+    function parse_ijadpanahsaravi_data()
+        """
+        Parses the dataset from Ijadpanahsaravi et al. (2023)
+        with multiple incolulum densities and returns a DataFrame.
+        """
+
+        df_germination = DataFrame(CSV.File("Data/swelling_germination_results.csv"; header=true))
+
+        # Filter the data to only include swelling
+        df_germination_swelling = filter(row -> row[1] == "Swelling", df_germination)
+
+        # Expression for parsing
+        regex_triplet = r"(-?\d+\.\d+)\[(-?\d+\.\d+);(-?\d+\.\d+)\]*.*"
+
+        # Function to parse a string and return three numbers
+        function parse_numbers(s)
+            m = match(regex_triplet, s)
+            if m !== nothing
+                num1 = parse(Float64, m.captures[1])
+                num2 = parse(Float64, m.captures[2])
+                num3 = parse(Float64, m.captures[3])
+                return num1, num2, num3
+            else
+                return missing, missing, missing
+            end
+        end
+
+        # Parse Pmax and its confidence intervals
+        Pmax_parsed = [parse_numbers(row[1]) for row in eachrow(df_germination_swelling[!, 4])]
+        Pmax_vals = [x[1] for x in Pmax_parsed]
+        PmaxCIlow_vals = [x[2] for x in Pmax_parsed]
+        PmaxCIhigh_vals = [x[3] for x in Pmax_parsed]
+
+        # Parse tau and its confidence intervals
+        tau_parsed = [parse_numbers(row[1]) for row in eachrow(df_germination_swelling[!, 5])]
+        tau_vals = [x[1] for x in tau_parsed]
+        tauCIlow_vals = [x[2] for x in tau_parsed]
+        tauCIhigh_vals = [x[3] for x in tau_parsed]
+
+        # Reconstruct the DataFrame with the parsed values
+        df_germination_rebuilt = DataFrame(
+            :CarbonSource => df_germination_swelling[!, 2],
+            :Density => inverse_um_to_mL(df_germination_swelling[!, 3] / 150),
+            :Pmax => Pmax_vals,
+            :Pmax_CI_Lower => PmaxCIlow_vals,
+            :Pmax_CI_Upper => PmaxCIhigh_vals,
+            :tau => tau_vals,
+            :tau_CI_Lower => tauCIlow_vals,
+            :tau_CI_Upper => tauCIhigh_vals,
+            :N => df_germination_swelling[!, 8],
+            :M => df_germination_swelling[!, 9]
+        )
+
+        df_germination_rebuilt
+    end
+end
